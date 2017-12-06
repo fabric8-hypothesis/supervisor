c499976bb6bf333af7f07c79748e3c43e030c06b
mdas <mdas@localhost.localdomain>
2017-12-06 17:10:04 +0530

    Removed unused variable in api_test.js

--- tests/api_test.js
+++ tests/api_test.js
@@ -6,7 +6,6 @@ var server   = require("../main"),
 chai.use(chaiHTTP)
 
 describe("Test data collection api", function() {
-	var host = "http://" + "0.0.0.0" + ":" + 9090
 
 	describe("POST to /api/v1.0/stats", () => {
 		it(" should return 200", function(done){
